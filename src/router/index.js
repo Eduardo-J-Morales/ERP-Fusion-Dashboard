77e8730457f389986c2d9a8f2d3e0b3a9e9d25e3
root <root@DESKTOP-RHJBB6F.>
2025-05-01 06:41:43 -0400

    Updates on router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,38 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '../stores/auth'
 
+const routes = [
+    {
+        path: '/login',
+        name: 'login',
+        component: () => import('../views/LoginView.vue')
+    },
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: () => import('../views/DashboardView.vue'),
+        meta: { requireAuth: true },
+        children: [
+            { path: 'inventory', component: () => import('../views/InventoryView.vue')},
+            { path: 'sales', component: () => import('../views/SalesView.vue')},
+            { path: 'employees', component: () => import('../views/EmployeesView.vue')},
+        ]
+    },
+    {path: '/', redirect: '/login'}
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+router.beforeEach((to, from, next) => {
+    const authStore = useAuthStore()
+    if (to.meta.requireAuth && !authStore.isAuthenticated) {
+        next('/login')
+    } else {
+        next()
+    }
+})
+
+export default router
