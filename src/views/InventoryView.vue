1ccd845679266077976861eac2989e5e52f93428
Eduardo J. Morales <eduardojosegonzalezmorales41@gmail.com>
2025-05-01 15:16:20 +0000

    Updates on src/views/InventoryViews.vue

--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -1,22 +1,41 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+const search = ref('')
+
 const headers = ref([
   { title: 'ID', key: 'id' },
-  { title: 'Name', key: 'name' },
-  { title: 'Quantity', key: 'quantity' },
-  { title: 'Price', key: 'price' },
-  { title: 'Category', key: 'category' },
+  { title: 'Name', key: 'name' }
 ])
 
 const items = ref([
   { id: 1, name: 'Laptop', quantity: 15, price: 999, category: 'Electronics' },
-  { id: 2, name: 'Desk Chair', quantity: 20, price: 199, category: 'Furniture' },
 ])
 
 </script>
 
 <template>
+    <v-card>
+        <v-card-title>
+            Inventory Managemnet
+            <v-spacer></v-spacer>
+            <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+            ></v-text-field>
+        </v-card-title>
+        <v-data-table
+            :headers="headers"
+            :items="items"
+            :search="search"
+            single-line
+            hide-details
+        >
+        </v-data-table>
+    </v-card>
 </template>
 
 <style scoped>
